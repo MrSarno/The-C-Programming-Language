49dcbf2aa3e8578a28dd6f9763bdab39645ec658
MrSarno <email@sarno.dev>
2022-11-21 13:13:03 +0000

    DEV: first commit

---
+++ Chapter 1 - A Tutorial Introduction/01 - Hello World/main.c
@@ -0,0 +1,6 @@
+#include <stdio.h> // include information about the standard library
+
+int main()// define a function named 'main' that receives no argument values
+{ // statements of main are enclosed in braces
+    printf("Hello, World!\n"); // main calls library function printf to print this sequence of characters
+}
